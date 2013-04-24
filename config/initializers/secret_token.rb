cc6f24c80800cf030143cf1b911852848f3c89ad
vaibhav puri <vaibhav@taggito.com>
2013-04-24 12:43:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TnwHackathon::Application.config.secret_token = '2b1b085f1918fdcaaa24fc724e06ac26e6a86b1569f1434a5a803322f0c4b957f3e8623d53de87de4459f0c800d14bbb31fb97b522f2020b7e6bce161c6bd432'
