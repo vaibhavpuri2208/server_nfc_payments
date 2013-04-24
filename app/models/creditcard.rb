b1d6e5f51fb2b3bef7e8904b7760ace33209f65c
vaibhav puri <vaibhav@taggito.com>
2013-04-24 22:12:04 +0200

    removed validation

--- app/models/creditcard.rb
+++ app/models/creditcard.rb
@@ -1,8 +1,8 @@
 class Creditcard < ActiveRecord::Base
-  validates :card_number, :numericality => true, :length => {is:16}
-  validates :cvv, :numericality => true, :length => {is:3}
-  validates :expiration_month, :numericality => true, :length => {is:2}
-  validates :expiration_year, :numericality => true, :length => {is:4}
+  validates :card_number, :numericality => true #, :length => {is:16}
+  validates :cvv, :numericality => true #, :length => {is:3}
+  validates :expiration_month, :numericality => true #, :length => {is:2}
+  validates :expiration_year, :numericality => true #, :length => {is:4}
   validates :billing_address, :presence => true
   attr_accessible :card_holder_name, :card_number, :cvv, :expiration_date, :expiration_month, :expiration_year, :billing_address, :customer_id
 end
